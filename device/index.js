757f7968b519cd1c45822915d796d6fc3b1fb502
Rahul Sahay <rahul.sahay@kdi.kongsberg.com>
2018-07-29 15:01:59 +0530

    initial commit

---
+++ device/index.js
@@ -0,0 +1,28 @@
+let deviceAmqp = require('azure-iot-device-amqp');
+let device = require('azure-iot-device');
+
+require('dotenv').config();
+let client = deviceAmqp.clientFromConnectionString(process.env.DEVICE_CONNECTION_STRING);
+console.log('connection string:-',process.env.DEVICE_CONNECTION_STRING);
+
+client.open(err => {
+    let deviceName = 'simdevice1';
+    console.log(`acting as ${deviceName}`);
+
+    //handle C2D messages
+    client.on('message', msg => {
+        client.complete(msg, () => console.log('<-- cloud message received'));
+    });
+
+    // send a D2C message repeatedly
+    setInterval(function () {
+        let message = new device.Message(JSON.stringify({
+            deviceId: deviceName,
+            value: Math.random()
+        }));
+        console.log('sending message to cloud -->');
+        client.sendEvent(message, (err,res) => {
+            if(err) console.log(err);
+        });
+    }, 5000);
+});
---
+++ service/index.js
@@ -0,0 +1,40 @@
+let iothub = require('azure-iothub');
+let common = require('azure-iot-common');
+let eventhubs = require('azure-event-hubs');
+
+require('dotenv').config();
+let connectionString = process.env.IOTHUB_CONNECTION_STRING;
+console.log('connection string:-',process.env.IOTHUB_CONNECTION_STRING);
+let eventhubsClient = eventhubs.Client.fromConnectionString(connectionString);
+let iothubClient = iothub.Client.fromConnectionString(connectionString);
+
+//open event hubs client for handling D2C messages
+eventhubsClient.open()
+    .then(() => console.log('ready'))
+    .then(() => eventhubsClient.getPartitionIds())
+    .then(pids =>
+        //enumerate the partitions
+        pids.map(pid =>
+            eventhubsClient.createReceiver('$Default', pid, { 'startAfterTime': Date.now() })
+                .then(receiver =>
+                    //handle D2C messages
+                    receiver.on('message', msg =>
+                        console.log('<-- device message received')
+                    )
+                )
+        )
+    );
+
+//open iothub client for handling C2D messages
+iothubClient.open(err => {
+    if(err) console.log('Error opening iothubClient: ' + err);
+    // send a C2D message repeatedly
+    setInterval(() => {
+        let device = 'simdevice1';
+        let message = new common.Message(JSON.stringify({ text: 'reset-request' }));
+        console.log(`sending message to ${device} -->`);
+        iothubClient.send(device, message, (err, res) => {
+            if (err) console.log(err);
+        });
+    }, 5000);
+});
